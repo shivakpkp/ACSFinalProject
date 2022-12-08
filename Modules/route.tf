5ff336cfa835f1248afcc2749d12ec9a67be27fc
pmartin66 <115438435+pmartin66@users.noreply.github.com>
2022-12-08 09:15:07 -0500

    Create route.tf

---
+++ Modules/route.tf
@@ -0,0 +1,15 @@
+###-----------------locals---------------------###
+
+locals { vm_subnets = { "1" : "${aws_subnet.subnet_block["2"].id}", "2" : "${aws_subnet.subnet_block["4"].id}", "3" : "${aws_subnet.subnet_block["6"].id}" } }
+locals { gw_subnets = { "1" : "${aws_subnet.subnet_block["1"].id}", "2" : "${aws_subnet.subnet_block["3"].id}" ,"3" : "${aws_subnet.subnet_block["5"].id}"} }
+
+###Route###
+resource "aws_route_table" "route_table_gw" {
+  vpc_id = aws_vpc.aws_vpc_name.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+}
