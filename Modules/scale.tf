fde9a189e3e15e9c47ac5e598fae44fe97d2a6e6
shivakpkp <96741085+shivakpkp@users.noreply.github.com>
2022-12-08 10:50:50 -0500

    completed scale file

--- Modules/scale.tf
+++ Modules/scale.tf
@@ -39,3 +39,62 @@ resource "aws_launch_configuration" "launchconfig" {
   }
   
 }
+
+
+# autoscale_group
+resource "aws_autoscaling_group" "scallingGroup" {
+  name                      = "scal_group"
+  max_size                  = 4
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  force_delete              = true
+  launch_configuration      = aws_launch_configuration.launchconfig.name
+  target_group_arns         = [aws_lb_target_group.target_group.arn]
+  # availability_zones        = [
+  # aws_subnet.subnet_block["2"].availability_zone,
+  # aws_subnet.subnet_block["4"].availability_zone,
+  # aws_subnet.subnet_block["6"].availability_zone]
+  vpc_zone_identifier = [
+    "${aws_subnet.subnet_block["2"].id}",
+    "${aws_subnet.subnet_block["4"].id}",
+  "${aws_subnet.subnet_block["4"].id}"]
+  
+  tag {
+    key = "Name"
+    value = "ec2_scale"
+    propagate_at_launch = true
+  }
+}
+
+
+# autoscale_policy
+
+resource "aws_autoscaling_policy" "auto_policy" {
+  name                   = "Scale_policy"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.scallingGroup.name
+}
+
+# autoscale cloud watch
+
+resource "aws_cloudwatch_metric_alarm" "custome_cpu_alarm" {
+  alarm_name = "cpu_alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = 2
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = 60
+  statistic = "Average"
+  threshold = 20
+  dimensions = {
+    "AutoScalingGroupName" : aws_autoscaling_group.scallingGroup.name
+  }
+  actions_enabled = true
+  
+  alarm_actions = [aws_autoscaling_policy.auto_policy.arn]
+}
+
